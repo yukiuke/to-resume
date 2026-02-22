52f0364a701dc72f986070fa37ac1cd827e26f4c
yukichan <baalsdepe@gmail.com>
2026-02-21 19:37:15 -0800

    Added favicon.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Fighting Game Events — BaalsDepe</title>
+    <link rel="icon" href="/favicon.png">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
